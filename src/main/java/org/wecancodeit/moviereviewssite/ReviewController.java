fd346a369a7ba24b32f5bfab1ac360375c352dcc
Bryan Long <bryancharleslong@gmail.com>
2019-02-13 05:58:27 -0500

    added favicon

--- src/main/java/org/wecancodeit/moviereviewssite/ReviewController.java
+++ src/main/java/org/wecancodeit/moviereviewssite/ReviewController.java
@@ -4,6 +4,7 @@ import javax.annotation.Resource;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -29,5 +30,10 @@ public class ReviewController {
 	public String findAllReviewWithTag(@RequestParam(value = "tag") String tag, Model model ) {
 		model.addAttribute("reviews", reviewRepo.findAllWithTag(tag));
 		return "tagReviews";
+	}
+	
+	@GetMapping("/")
+	public String getReviews() {
+	    return "redirect:/show-reviews";
 	}
 }
